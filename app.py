c68cd5426a26ae1fd929148c73fb3f188a532c7f
Sean Bannon <seannbannon@gmail.com>
2023-02-15 21:31:13 -0600

    adding a try and except to the post method. it should try to commit the input to its database -- if not i think it just displays what is already there

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,7 +19,21 @@ class Todo(db.Model):
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+# connects to the 'content' id in the index.html
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+# commit task to database
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+# looks at all the database contents in the order it was created and display them all
+            tasks = Todo.query.order_by(Todo.date_created).all()
+            return 'There was an issue adding your task.'
+        
+
     else:
         return render_template('index.html')
 

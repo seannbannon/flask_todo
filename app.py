ddf9db60d330e3539cf1fd9c833dbbe4442fc724
Sean Bannon <seannbannon@gmail.com>
2023-02-15 22:16:43 -0600

    fixed date error, now you can post a new task

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def index():
             db.session.commit()
             return redirect('/')
         except:
-# looks at all the database contents in the order it was created and display them all
-            tasks = Todo.query.order_by(Todo.date_created).all()
             return 'There was an issue adding your task.'
         
 
     else:
+        # looks at all the database contents in the order it was created and display them all
+        tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
